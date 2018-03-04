23669c0960eb6aa71243b578fca0143f3c2bdabb
clfer <clement.ferrier+github@gmail.com>
2018-03-04 23:05:13 +0100

    Added accordion for config page

--- app/pages.php
+++ app/pages.php
@@ -1,10 +1,11 @@
 <?php
+
 use TabModifierCompanion\Model\Config;
 
 /**
  * @return array
  */
-function page_icon_generation(){
+function page_icon_generation() {
   $conf_path = './conf/example.conf.json';
   $icons = icon_generation($conf_path);
   $content['body'][] = _print_icons_table($icons, TRUE);
@@ -15,11 +16,23 @@ function page_icon_generation(){
 /**
  * @return array
  */
-function page_conf(){
+function page_conf() {
   $conf_path = './conf/example.conf.json';
   Config::load($conf_path);
 
-  $content['body'][] = Config::toHtml();
+  $id = 'config-accordion';
+  $panels['config'] = [
+    'title' => 'Config',
+    'content' => Config::toHtml(),
+  ];
+  $panels['json'] = [
+    'title' => 'Json',
+    'content' => '<pre>' . Config::toJson() . '</pre>',
+  ];
+
+  ob_start();
+  include 'templates/accordion.tpl.php';
+  $content['body'][] = ob_get_clean();
 
   return $content;
 }
