0756e8f305ccf4c4846a7ca6da2dc259ec4bdaa3
clfer <clfer@users.noreply.github.com>
2018-03-07 16:23:50 +0100

    Implemented VariationColor

--- app/model/Variation/VariationColor.php
+++ app/model/Variation/VariationColor.php
@@ -4,12 +4,35 @@
 namespace TabModifierCompanion\Model\Variation;
 
 
+use ColorThief\ColorThief;
+use TabModifierCompanion\Model\Config;
+
 class VariationColor extends Variation {
 
   public $type = 'color';
 
   function apply($image_path) {
-    $variation_path = $image_path;
-    parent::applySubvariations($variation_path);
+
+    if (empty($this->options['color'])) {
+      throw new \Exception(__CLASS__ . '->' . __FUNCTION__ . ' : Missing \'color\' options.');
+    }
+
+    list($r, $g, $b) = ColorThief::getColor($image_path, 1);
+    list($base_hue, $base_saturation, $base_value) = rgb2hsl($r, $g, $b);
+
+    $base_layer = imagecreatefrompng($image_path);
+    _imagetransparency($base_layer);
+    list($colorize_hue, $colorize_saturation, $colorize_value) = hex2hsl($this->options['color']);
+    $delta_hue = $colorize_hue - $base_hue;
+    $delta_value = $colorize_value - $base_value;
+    imagehue($base_layer, $delta_hue * 360, NULL, $delta_value);
+
+    // ?
+    $variation_filename = pathinfo($image_path, PATHINFO_FILENAME) . '-' . $this->label;
+
+    $variation_path = imagepng_save($base_layer, Config::getIconsGenerationDir(), $variation_filename);
+
+    // Apply subvariations
+    parent::apply($variation_path);
   }
 }
